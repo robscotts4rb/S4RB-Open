799372219097b7a85e4be24e139cc4635a9d9372
Adrian Eyre <adrian.eyre@hotmail.co.uk>
2017-05-15 12:21:05 +0100

    Updated month view to display missing data

--- Javascript/src/views/monthly.js
+++ Javascript/src/views/monthly.js
@@ -3,12 +3,19 @@ function monthyView(response, monthlyCPMU = new CPMU()){
   $('#title').text("Montly - Complaints per million");
   $('#toggle-button').text("Quarterly");
 
+  var nextDate = new Date(response[0].Month);
   text = "<table border='1'><tr><td width='200'><b>Date</b></td><td width='200'><b>CPMU</b></td></tr>";
   for(var i=0; i<response.length; i++){
+    while (new Date(response[i].Month).toDateString() !== new Date(nextDate).toDateString()) {
+      text += "<tr><td width='200'>"+monthlyCPMU.display_date(nextDate)+"</td><td width='200'>0.00000</td></tr>";
+      nextDate = new Date(nextDate.setMonth(nextDate.getMonth() + 1));
+    }
     var val1 = response[i].Complaints;
     var val2 = response[i].UnitsSold;
-    var val3 = response[i]["Value.date"];
+    var val3 = response[i].Month;
     text += "<tr><td width='200'>"+monthlyCPMU.display_date(val3)+"</td><td width='200'>"+monthlyCPMU.calculate(val1, val2)+"</td></tr>";
+    nextDate = new Date(response[i].Month);
+    nextDate = new Date(nextDate.setMonth(nextDate.getMonth() + 1));
   }
 
   document.getElementById('table').innerHTML = text+"</table>";
